3039f1fb8a7af612a820e3305457b868bce47c72
Adalberto Hofmann <adalberto@css4html.com.br>
2019-10-18 18:31:35 -0300

    [master]Creating relationships

--- app/models/available_product.rb
+++ app/models/available_product.rb
@@ -1,2 +1,8 @@
 class AvailableProduct < ApplicationRecord
+  belongs_to :product
+  belongs_to :color
+  belongs_to :size
+
+  validates :product_id, :color_id, :size_id, presence: true
+
 end
--- app/models/size.rb
+++ app/models/size.rb
@@ -1,2 +1,3 @@
 class Size < ApplicationRecord
+  has_many :available_products
 end
